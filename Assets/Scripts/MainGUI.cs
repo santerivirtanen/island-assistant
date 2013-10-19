50172f0bbed71cc6999dc56fbb05fefa14339ac8
Santeri Virtanen <santeri.virtanen@edu.karelia.fi>
2013-10-19 14:54:28 +0300

    Added main scene with simple GUI

---
+++ Assets/Scripts/MainGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MainGUI : MonoBehaviour {
+
+	void OnGUI() 
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Hello World!");
+		GUILayout.EndHorizontal();
+	
+	}
+}
